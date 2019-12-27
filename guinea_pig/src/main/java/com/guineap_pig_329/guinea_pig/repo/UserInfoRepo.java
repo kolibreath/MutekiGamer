3d593fca1b2c39609d28a56b69adf12fbc32e485
RawSharkill <1229002314@qq.com>
2019-12-27 10:26:24 +0800

    [merge]

--- guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/repo/UserInfoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.guineap_pig_329.guinea_pig.repo;
-
-import com.guineap_pig_329.guinea_pig.dao.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserInfoRepo extends JpaRepository<UserInfo,Integer> {
-}
