a7c12569fa2aa4ab3a31f51ab12f9be2d4eb087f
RawSharkill <1229002314@qq.com>
2019-12-27 17:16:51 +0800

    [add]

--- guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/GuineaPigApplication.java
+++ guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/GuineaPigApplication.java
@@ -2,19 +2,10 @@ package com.guineap_pig_329.guinea_pig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
 @SpringBootApplication
 public class GuineaPigApplication {
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(GuineaPigApplication.class, args);
-//    }
-    @RequestMapping("/index")
-    public String index()
-    {
-        return "index";
+    public static void main(String[] args) {
+        SpringApplication.run(GuineaPigApplication.class, args);
     }
 }
