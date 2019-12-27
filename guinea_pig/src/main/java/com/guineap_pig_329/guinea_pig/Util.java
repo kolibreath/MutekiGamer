7abcfe2167b56115e7feb80230c4a2203027c69f
kolibreath <734780178@qq.com>
2019-12-27 17:02:15 +0800

    [ADD]

--- guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/Util.java
+++ guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/Util.java
@@ -1,17 +1,5 @@
 package com.guineap_pig_329.guinea_pig;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.guineap_pig_329.guinea_pig.dao.ManagementElement;
-import com.guineap_pig_329.guinea_pig.dao.Plate;
-import com.guineap_pig_329.guinea_pig.dao.User;
-import com.guineap_pig_329.guinea_pig.dao.UserInfo;
-import com.guineap_pig_329.guinea_pig.repo.PlateRepo;
-import com.guineap_pig_329.guinea_pig.repo.UserInfoRepo;
-import com.guineap_pig_329.guinea_pig.repo.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 public class Util {
 
 
