7bb9e3b9dc338718a2e63298854f747c9d73d50e
kolibreath <734780178@qq.com>
2020-01-06 20:25:12 +0800

    [ADD] battle/new/id

---
+++ guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/controller/FileUpLoadController.java
@@ -0,0 +1,11 @@
+package com.guineap_pig_329.guinea_pig.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("upload/")
+public class FileUpLoadController {
+
+
+//    @PostMapping("img")
+
+}
