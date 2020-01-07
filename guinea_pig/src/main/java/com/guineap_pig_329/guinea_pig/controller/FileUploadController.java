d309f67407c15efc7ad8db8485497a8bf4971345
kolibreath <734780178@qq.com>
2020-01-07 10:27:06 +0800

    [ADD] 添加队员数据

---
+++ guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/controller/FileUploadController.java
@@ -0,0 +1,6 @@
+package com.guineap_pig_329.guinea_pig.controller;
+
+//http://www.ityouknow.com/springboot/2018/01/12/spring-boot-upload-file.html
+//前端需要指定multiple part
+public class FileUploadController {
+}
---
+++ guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/dao/Player.java
@@ -0,0 +1,12 @@
+package com.guineap_pig_329.guinea_pig.dao;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "player")
+public class Player {
+
+//    @Id
+//    @GeneratedValue
+}
