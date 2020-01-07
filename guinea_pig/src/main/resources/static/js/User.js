c3f8f4c636a57a7dcdd2d34207e9e8bfc05eb423
kolibreath <734780178@qq.com>
2020-01-07 15:49:25 +0800

    [ADD]

--- guinea_pig/src/main/resources/static/js/User.js
+++ guinea_pig/src/main/resources/static/js/User.js
@@ -55,7 +55,7 @@ function getUserInfo(call) {
         type:"GET",
         url:"/user/info",
         success:function (result) {
-            allData.info = result;
+            allData = result;
         },
         error:function () {
             //todo 错误处理
@@ -69,14 +69,13 @@ function getUserInfo(call) {
 function sendEmail(call){
     let allData={};
 
-    alert("fuck 豚鼠邮箱");
 
     let request = $.ajax(({
         type:"GET",
         url:"/mail/send",
         success:function (result) {
             alert("fuck 豚鼠大邮箱");
-            allData.info = result;
+            allData = result;
         },
         error:function (result) {
             alert("fuck 豚鼠大邮箱" + result);
