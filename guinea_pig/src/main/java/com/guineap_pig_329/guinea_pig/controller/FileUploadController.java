360438f2401bec05dc47f28c30dd0f3ba47c92aa
kolibreath <734780178@qq.com>
2020-01-07 15:47:15 +0800

    [add] picture

--- guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/controller/FileUploadController.java
+++ guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/controller/FileUploadController.java
@@ -3,12 +3,13 @@ package com.guineap_pig_329.guinea_pig.controller;
 //http://www.ityouknow.com/springboot/2018/01/12/spring-boot-upload-file.html
 //前端需要指定multiple part
 
-import com.guineap_pig_329.guinea_pig.dao.ResultBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
 
 @Controller("upload/")
 public class FileUploadController {
@@ -16,11 +17,20 @@ public class FileUploadController {
     @PostMapping("img")
     //和表单中的key 对应
     //上传完成之后会重定向
-    public String uploadPic(@RequestParam("file")MultipartFile file, RedirectAttributes redirectAttributes){
+    public String uploadPic(@RequestParam("avatar")MultipartFile avatar, RedirectAttributes redirectAttributes){
 
-        if(file.isEmpty()){
+        if(avatar.isEmpty()){
             redirectAttributes.addFlashAttribute("message","Please select file to upload");
-
+            //todo  上传图片的部分
+            return"redirect:/";
         }
+
+        try {
+            byte[] bytes = avatar.getBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        Path path = Paths.get("muteki_gamer/"+f)
+        return null;
     }
 }
