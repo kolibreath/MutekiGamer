f18984bae1e5ec99ce0b7def41aa543a62103a00
kolibreath <734780178@qq.com>
2020-01-07 16:45:02 +0800

    [ADD] fix bug

--- guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/controller/FileUploadController.java
+++ guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/controller/FileUploadController.java
@@ -3,9 +3,10 @@ package com.guineap_pig_329.guinea_pig.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("upload/")
-public class FileUpLoadController {
+public class FileUploadController {
 
 
 //    @PostMapping("img")
+
 
 }
