01085ca01caab27cc3652a3e19b8e9911145570a
RawSharkill <1229002314@qq.com>
2020-01-11 10:18:42 +0800

    [Delete] FileUpLoadController

--- guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/controller/FileUploadController.java
+++ guinea_pig/src/main/java/com/guineap_pig_329/guinea_pig/controller/FileUploadController.java
@@ -3,7 +3,7 @@ package com.guineap_pig_329.guinea_pig.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("upload/")
-public class FileUploadController {
+public class FileUpLoadController {
 
 
 //    @PostMapping("img")
